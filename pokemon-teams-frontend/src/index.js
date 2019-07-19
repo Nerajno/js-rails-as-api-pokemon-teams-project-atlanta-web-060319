4d6f47b23852b2938e39940fc8d6f7f0b6024897
Nerajno <Nerajno@gmail.com>
2019-07-19 15:07:19 -0400

    dye prying

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,24 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemom`
+const POKEMONS_URL = `${BASE_URL}/pokemon`
+
+document.addEventListener("DOMContentLoaded", setUpPage)
+
+function setUpPage(){
+    
+    const div = document.createElement("div", className="card")
+
+    const addPokeBtn = document.createElement("BUTTON", id="btn")
+
+    const ul = document.createElement("ul")
+
+    div.appendChild(addPokeBtn)
+    div.appendChild(ul)
+}
+
+
+function getAllPokemon(){
+    fetch(POKEMONS_URL)
+    .then(res => res.json())
+    .then(data => createPokemonList)
+}
